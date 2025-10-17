acd30bcf3f08cc4934e37087e6dd9bd9b03c7cb8
ANU YADAV <163430575+yadavanu63@users.noreply.github.com>
2025-10-17 20:56:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_URL = getenv("MONGO_URL")
 
 OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tapori420_Official")
+
